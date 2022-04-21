c64222d8cf235cca49406fe59923970f800e8441
Victor Costa <vg47683@gmail.com>
2022-04-21 12:19:05 -0300

    Creating contacts

---
+++ src/controllers/contactController.js
@@ -0,0 +1,36 @@
+const Contact = require('../models/ContactModel');
+
+exports.index = (req, res) => {
+  res.render('contact', {
+    contact: {}
+  });
+};
+
+exports.register = async(req, res) => {
+  try{
+    const contact = new Contact(req.body);
+    await contact.register();
+
+    if(contact.errors.length > 0) {
+      req.flash('errors', contact.errors);
+      req.session.save(() => res.redirect('back'));
+      return;
+    }
+
+    req.flash('success', 'Contato registrado com sucesso.');
+    req.session.save(() => res.redirect(`/contact/index/${contact.contact._id}`));
+  } catch(e) {
+    console.log(e);
+    return res.render('404');
+  }
+  
+};
+
+exports.editIndex = async (req, res) => {
+  if(!req.params.id) return res.render('404');
+  
+  const contact = await Contact.searchById(req.params.id);
+  if(!contact) return res.render('404');
+
+  res.render('contact', { contact });
+};
